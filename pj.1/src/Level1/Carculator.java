882ee336720945f91411c133e48ad50dc4718177
Seokhyeonh <166996811+Seokhyeonh@users.noreply.github.com>
2024-04-29 21:49:58 +0900

    5. 연산 결과 저장

--- pj.1/src/Level1/Carculator.java
+++ pj.1/src/Level1/Carculator.java
@@ -1,5 +1,7 @@
 package Level1;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Carculator {
@@ -8,6 +10,10 @@ public class Carculator {
         double result =0 ;
         String out = "exit";
 
+        /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
+        /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
+        int[] arr = new int[10];
+        int count = 0;
 
         while (true) {
             System.out.println("첫 번째 숫자를 입력하세요: ");
@@ -16,11 +22,6 @@ public class Carculator {
             char ch = sc.next().charAt(0);
             System.out.println("두 번째 숫자를 입력하세요: ");
             double num2 = sc.nextDouble();
-
-
-
-
-
 
 
 
@@ -41,8 +42,12 @@ public class Carculator {
                         System.out.println("나눗셈 연산에서 분모에는 0을 입력 할 수 없습니다");
                     }
                     break;
+                case '%':
 
             }
+            arr[count] =(int) result;
+            count +=1;
+
             System.out.println("결과: " + result);
             System.out.println("exit 입력 시 종료합니다");
             String input = sc.next();
