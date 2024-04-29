d4cbf07edddc063c82c91b2c042161201683755e
Seokhyeonh <166996811+Seokhyeonh@users.noreply.github.com>
2024-04-29 15:51:06 +0900

    사칙연산 기호 입력

--- pj.1/src/Level1/Carculator.java
+++ pj.1/src/Level1/Carculator.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class Carculator {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력하세요");
+        System.out.println("첫 번째 숫자를 입력하세요: ");
         double num1 =sc.nextDouble();
-        System.out.println("두 번째 숫자를 입력하세요");
+        System.out.println("두 번째 숫자를 입력하세요: ");
         double num2 =sc.nextDouble();
+
+        System.out.println("사칙 연산기호을 입력하세요: ");
+        char ch =sc.next().charAt(0);
+
+
+
 
 
 
