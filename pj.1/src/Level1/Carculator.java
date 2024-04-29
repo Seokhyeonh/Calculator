0cf8e312f0fe5336a1f5c99389c1dc67ec811ce7
Seokhyeonh <166996811+Seokhyeonh@users.noreply.github.com>
2024-04-29 15:33:39 +0900

    level 1

---
+++ pj.1/src/Level1/Carculator.java
@@ -0,0 +1,19 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class Carculator {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력하세요");
+        double num1 =sc.nextDouble();
+        System.out.println("두 번째 숫자를 입력하세요");
+        double num2 =sc.nextDouble();
+
+
+
+    }
+
+
+
+}
